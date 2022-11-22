04f2f45f132da9e49c37aa1db3c60f321d514317
Acer <Acer@GAETANO>
2022-11-22 19:22:13 +0100

    Gaetano Shapes classes

---
+++ DrawingApp/src/Shapes/ConcreteShapeRectangles.java
@@ -0,0 +1,66 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Shapes;
+
+/**
+ *
+ * @author Acer
+ */
+import javafx.scene.control.ColorPicker;
+import javafx.scene.shape.Rectangle;
+
+public class ConcreteShapeRectangles {
+    private ColorPicker FillColor, LineColor;
+    public double width, height, PositionX, PositionY;
+
+    private Rectangle rectangle = new Rectangle();
+    
+    public void setLineColor(ColorPicker colorPicker){
+        this.LineColor = colorPicker;
+    }
+
+    public void setFillColor(ColorPicker colorPicker){
+        this.FillColor = colorPicker;
+    }
+
+    public void setWidth(){
+        this.width = 4;
+
+        rectangle.setWidth(Math.abs((width)));
+    }
+
+    public void setHeight(){
+        this.height = 2;
+
+        rectangle.setHeight(height);
+    }
+
+   
+    public double getPositionX(){
+        return rectangle.getX();
+    }
+
+    public double getPositionY(){
+        return rectangle.getY();
+    }
+
+    public double getWidth(){
+        return rectangle.getWidth();
+    }
+
+    public double getHeight() {
+        return rectangle.getHeight();
+    }
+
+    public ColorPicker getLineColor(){
+        return LineColor;
+    }
+
+    public ColorPicker getFillColor(){
+        return FillColor;
+    }
+   
+
+}
--- DrawingApp/src/workspace/WorkspaceController.java
+++ DrawingApp/src/workspace/WorkspaceController.java
@@ -56,6 +56,7 @@ public class WorkspaceController implements Initializable {
 
     @FXML
     private void saveProject(ActionEvent event) { //metodo per salvare il progetto
+        
     }
 
     @FXML
