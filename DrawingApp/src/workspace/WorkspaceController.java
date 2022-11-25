0485d8453dc3f1397fa6a04271e54d2a835e9322
Acer <Acer@GAETANO.mshome.net>
2022-11-25 16:38:19 +0100

    Gaetano Canvas update

--- DrawingApp/src/workspace/WorkspaceController.java
+++ DrawingApp/src/workspace/WorkspaceController.java
@@ -76,7 +76,7 @@ public class WorkspaceController implements Initializable {
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        
     } 
     
     private void loadWindow(String location, String title) throws IOException{ //metodo per far apparire una nuova finestra. Usato per la creazione di nuovi progetti
@@ -109,7 +109,7 @@ public class WorkspaceController implements Initializable {
             }
         }
     }
-
+     
     @FXML
     private void saveProject(ActionEvent event) throws IOException { //metodo per salvare il progetto
         FileChooser save = new FileChooser();
@@ -129,6 +129,7 @@ public class WorkspaceController implements Initializable {
     @FXML
     private void MakeDraw(MouseEvent event){
         gc = drawingCanvas.getGraphicsContext2D();
+        
         if (mod == "Line"){
             Line = new ConcreteCreatorLine();
             line = Line.createShape();
