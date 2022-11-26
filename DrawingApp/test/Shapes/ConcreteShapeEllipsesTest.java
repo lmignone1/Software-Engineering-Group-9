6f04733195d6f47c63806383552d74e2210596ab
Lorenzo Mignone <l.mignone1>
2022-11-26 14:09:34 +0100

    test with rand2

--- DrawingApp/test/Shapes/ConcreteShapeEllipsesTest.java
+++ DrawingApp/test/Shapes/ConcreteShapeEllipsesTest.java
@@ -6,6 +6,7 @@ package Shapes;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.PrimitiveIterator;
 import java.util.Random;
 import java.util.stream.DoubleStream;
 import javafx.embed.swing.JFXPanel;
