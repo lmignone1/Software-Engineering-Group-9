60658580aac3b892b38300b78afc4ef6aef188be
Davide Parente <Davide@DESKTOP-944J1L0.homenet.telecomitalia.it>
2022-12-11 22:57:00 +0100

    Davide, MemoryTest

--- DrawingApp/test/Memory/MemoryTest.java
+++ DrawingApp/test/Memory/MemoryTest.java
@@ -4,9 +4,20 @@
  */
 package Memory;
 
+import Command.DeleteCommand;
+import Command.Select;
+import Factory.Creator;
 import Shapes.Shape;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.PrimitiveIterator;
+import java.util.Random;
 import java.util.Stack;
+import java.util.stream.DoubleStream;
+import javafx.embed.swing.JFXPanel;
+import javafx.scene.canvas.Canvas;
 import javafx.scene.control.ColorPicker;
+import javafx.scene.paint.Color;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -20,7 +31,111 @@ import static org.junit.Assert.*;
  */
 public class MemoryTest {
     
+    
+    private Memory instance;
+    private JFXPanel panel;
+    
+    //SELECT ATTRIBUTE
+    private Select selectedShape;
+    private Shape selectShape;
+    private List<Shape> listShape;
+    //CREATION SHAPE ATTRIBUTE
+    private Creator creator;
+    private Canvas canvas;
+    private List<ColorPicker> listColor;
+    private ColorPicker colorPickerWhite;
+    private ColorPicker colorPickerRed;
+    private ColorPicker colorPickerBlue;
+    private ColorPicker colorPickerYellow;
+    private ColorPicker colorPickerOrange;
+    private ColorPicker colorPickerGreen;
+    private ColorPicker colorPickerPurple;
+    private ColorPicker colorPickerBlack;
+    private List<String> type;
+    private final int NUM = 10;
+    private Shape createdShape;
+    //RANDOM
+    private Random rand;
+    private double[] vect;
+    private DoubleStream stream;
+    private int count;
+    private PrimitiveIterator.OfDouble it;
+    private double[] degreesVect;
+
+    
     public MemoryTest() {
+        
+        degreesVect = new double[100];
+        panel = new JFXPanel();
+        listShape = new ArrayList<>();
+        selectShape = null;
+        selectedShape = new Select(listShape, selectShape);
+        creator = new Creator();
+        canvas = new Canvas(1400, 1000);
+
+        listColor = new ArrayList<>();
+
+        colorPickerWhite = new ColorPicker(Color.WHITE);
+        colorPickerRed = new ColorPicker(Color.RED);
+        colorPickerBlue = new ColorPicker(Color.BLUE);
+        colorPickerYellow = new ColorPicker(Color.YELLOW);
+        colorPickerOrange = new ColorPicker(Color.ORANGE);
+        colorPickerGreen = new ColorPicker(Color.GREEN);
+        colorPickerPurple = new ColorPicker(Color.PURPLE);
+        colorPickerBlack = new ColorPicker(Color.BLACK);
+
+        listColor.add(colorPickerWhite);
+        listColor.add(colorPickerRed);
+        listColor.add(colorPickerBlue);
+        listColor.add(colorPickerYellow);
+        listColor.add(colorPickerOrange);
+        listColor.add(colorPickerGreen);
+        listColor.add(colorPickerPurple);
+        listColor.add(colorPickerBlack);
+
+        type = new ArrayList<>();
+        type.add("Line");
+        type.add("Rectangle");
+        type.add("Ellipse");
+        type.add("Text");
+
+        int leftLimit = 97; //letter a
+        int rightLimit = 122; //letter z
+        int targetStringLength = 10;
+        Random random = new Random();
+
+        rand = new Random();
+        vect = new double[100];
+        stream = rand.doubles(-999.999, 999.999);
+        count = 0;
+        it = stream.iterator();
+        while (count < vect.length && it.hasNext()) {
+            vect[count] = it.nextDouble();
+            count++;
+        }
+
+        count = 0;
+        stream = rand.doubles(-360.0, 360.001);
+        it = stream.iterator();
+        while (count < degreesVect.length && it.hasNext()) {
+            degreesVect[count] = it.nextDouble();
+            count++;
+        }
+
+        for (int i = 0; i < NUM; i++) {
+            String generatedString = random.ints(leftLimit, rightLimit + 1)
+                    .limit(targetStringLength)
+                    .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
+                    .toString();
+
+            createdShape = Creator.createShape(type.get(rand.nextInt(type.size())),
+                    canvas.getGraphicsContext2D(), vect[rand.nextInt(vect.length)],
+                    vect[rand.nextInt(vect.length)], listColor.get(rand.nextInt(listColor.size())),
+                    listColor.get(rand.nextInt(listColor.size())), vect[rand.nextInt(vect.length)],
+                    vect[rand.nextInt(vect.length)], generatedString, degreesVect[rand.nextInt(degreesVect.length)]);
+            listShape.add(createdShape);
+        }
+  
     }
     
     @BeforeClass
@@ -33,6 +148,7 @@ public class MemoryTest {
     
     @Before
     public void setUp() {
+        instance = new Memory();
     }
     
     @After
